c15cac9a44bfb5ba00fa87e01653d731c10fc26d
funaki302 <rafanomezantsoafunakilive@gmail.com>
2025-12-17 21:24:19 +0300

    view vaovao

--- Data/view.sql
+++ Data/view.sql
@@ -4,12 +4,11 @@ SELECT
     SUM(c.kilos * t.prix) AS recette,
     SUM(ch.salaires_parLiv) AS salaire_chauffeur,
     SUM(cb.prix_litre) AS cout_carburant,
-    (SUM(c.kilos * t.prix) 
-     - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
+    (SUM(c.kilos * t.prix) - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
 FROM gc_livraison l
 JOIN gc_colis c ON l.id_colis = c.id_colis
-JOIN gc_chauffeur ch ON ch.id_livraison = l.id_livraison
-JOIN gc_voiture v ON ch.id_voiture = v.id_voiture
+JOIN gc_chauffeur ch ON l.id_chauffeur = ch.id_chauffeur
+JOIN gc_voiture v ON l.id_voiture = v.id_voiture
 JOIN gc_carburant cb ON v.id_carburant = cb.id_carburant
 JOIN gc_tarifs t ON t.unite = 'kg'
 WHERE l.id_statut = 2
@@ -23,17 +22,15 @@ SELECT
     SUM(c.kilos * t.prix) AS recette,
     SUM(ch.salaires_parLiv) AS salaire_chauffeur,
     SUM(cb.prix_litre) AS cout_carburant,
-    (SUM(c.kilos * t.prix) 
-     - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
+    (SUM(c.kilos * t.prix) - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
 FROM gc_livraison l
 JOIN gc_colis c ON l.id_colis = c.id_colis
-JOIN gc_chauffeur ch ON ch.id_livraison = l.id_livraison
-JOIN gc_voiture v ON ch.id_voiture = v.id_voiture
+JOIN gc_chauffeur ch ON l.id_chauffeur = ch.id_chauffeur
+JOIN gc_voiture v ON l.id_voiture = v.id_voiture
 JOIN gc_carburant cb ON v.id_carburant = cb.id_carburant
 JOIN gc_tarifs t ON t.unite = 'kg'
 WHERE l.id_statut = 2
 GROUP BY YEAR(l.date_livraison), MONTH(l.date_livraison);
-
 
 CREATE OR REPLACE VIEW V_gc_BeneficeAnnee AS
 SELECT
@@ -41,12 +38,11 @@ SELECT
     SUM(c.kilos * t.prix) AS recette,
     SUM(ch.salaires_parLiv) AS salaire_chauffeur,
     SUM(cb.prix_litre) AS cout_carburant,
-    (SUM(c.kilos * t.prix) 
-     - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
+    (SUM(c.kilos * t.prix) - (SUM(ch.salaires_parLiv) + SUM(cb.prix_litre))) AS benefice
 FROM gc_livraison l
 JOIN gc_colis c ON l.id_colis = c.id_colis
-JOIN gc_chauffeur ch ON ch.id_livraison = l.id_livraison
-JOIN gc_voiture v ON ch.id_voiture = v.id_voiture
+JOIN gc_chauffeur ch ON l.id_chauffeur = ch.id_chauffeur
+JOIN gc_voiture v ON l.id_voiture = v.id_voiture
 JOIN gc_carburant cb ON v.id_carburant = cb.id_carburant
 JOIN gc_tarifs t ON t.unite = 'kg'
 WHERE l.id_statut = 2
